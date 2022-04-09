2e4cfeb275eccf93fc7e3d13cdaa5d0214cf7215
Jan William Jensen <69847071+572018@users.noreply.github.com>
2022-04-09 14:32:00 +0200

    Add project files.

---
+++ Api/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using Api;
+using Microsoft.AspNetCore.Mvc;
+using Oblig3.Services;
+
+namespace Oblig3.Controllers
+{
+    [Route("api/student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        //public StudentService _studentService;
+        public dat154Context _context = new dat154Context();
+
+        //public StudentController(dat154Context context)
+        //{
+        //    //_studentService = studentService;
+        //    _context = context;
+        //}
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public ActionResult<Student> GetAllStudents()
+        {
+            var s = _context.Students.ToList();
+            if(s == null)
+            {
+                return BadRequest();
+            }
+            return Ok(s);
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Api/Services/StudentService.cs
@@ -0,0 +1,20 @@
+﻿
+using Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Oblig3.Services
+{
+    public class StudentService
+    {
+
+        //public ActionResult<IEnumerable<Student>> GetAllStudent()
+        //{
+              
+        //}
+
+        //public Student? GetStudent(int id)
+        //{
+            
+        //}
+    }
+}
